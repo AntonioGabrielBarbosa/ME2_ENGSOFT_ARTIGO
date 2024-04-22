6b23e40fa076d37c3509a7407c8dcf4a269bf1ba
AntonioGabrielBarbosa <antonio.gabrielbarbosa2003@hotmail.com>
2024-04-22 11:08:51 -0300

    Teste

---
+++ Knowledgekey/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA6yHr-xIuuga9isPeLnUoyh2578Ts3W0Y",
+  authDomain: "knowledgekey-6cbe7.firebaseapp.com",
+  projectId: "knowledgekey-6cbe7",
+  storageBucket: "knowledgekey-6cbe7.appspot.com",
+  messagingSenderId: "753129151747",
+  appId: "1:753129151747:web:a7fcd309d99f788d459d36",
+  measurementId: "G-TF9SB5NB75"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);

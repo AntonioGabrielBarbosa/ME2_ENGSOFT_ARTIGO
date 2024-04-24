384783177fd14d4612b5cf3a23326f6204f27d83
AntonioGabrielBarbosa <antonio.gabrielbarbosa2003@hotmail.com>
2024-04-24 15:04:56 -0300

    Criando Banco de dados para registro e login do site

--- Knowledgekey/registro.js
+++ Knowledgekey/registro.js
@@ -1,8 +1,28 @@
 import { getDatabase, ref, set } from "firebase/database";
 
-function writeUserData(userID, name, email, password){
+function writeUserData(nome, email, senha, dataNascimento){
 
     const db = getDatabase();
-    set(ref())
+    set(ref(db, 'users/' + email), {
 
+        dataNascimento: dataNascimento,
+        email: email,
+        nome: nome,
+        senha: senha,
+    });
+
+}
+
+function writeAdminData(nome, email, senha, dataNascimento) {
+
+    const db = getDatabase();
+    set(ref(db, 'admins/' + email), {
+
+        dataNascimento: dataNascimento,
+        email: email,
+        nome: nome,
+        senha: senha,
+
+    });
+    
 }
